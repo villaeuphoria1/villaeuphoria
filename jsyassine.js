54192f5d33359cfd24c134656c92fc468a3e71d6
villaeuphoria1 <yassinebelebbayda@gmail.com>
2024-12-19 23:48:22 +0100

    Add files via upload

---
+++ jsyassine.js
@@ -0,0 +1,8 @@
+function openLightbox(img) {
+    document.getElementById("lightbox").style.display = "block";
+    document.getElementById("lightbox-img").src = img.src;
+}
+
+function closeLightbox() {
+    document.getElementById("lightbox").style.display = "none";
+}
